b15ea87a06416f94d2ecbc52bec27137e6c6e944
iamhyc <sudofree@163.com>
2018-03-01 22:13:03 +0800

    test: learnig concept

--- main.cc
+++ main.cc
@@ -1,18 +1,10 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
 /*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+{Node, Channel, Device, Stack, Address, Interface, Service, Application}
+Stack -->Node
+Channel -->Node = DeviceContainer
+Service -->Node = ApplicationContainer
+*/
 
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
@@ -22,36 +14,36 @@
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
+NS_LOG_COMPONENT_DEFINE ("HybridVirtualRouting");
 
-int
-main (int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
+  /* Command Parameter Parse */
   CommandLine cmd;
   cmd.Parse (argc, argv);
-  
-  Time::SetResolution (Time::NS);
-  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
+  /* 1.Node Container Setup */
   NodeContainer nodes;
   nodes.Create (2);
 
+  /* 2.Channel Helper Setup */
   PointToPointHelper pointToPoint;
   pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
   pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
+  /* 1+2 Channel->Node */
   NetDeviceContainer devices;
   devices = pointToPoint.Install (nodes);
 
+  /* 3.Internet Stack Install */
   InternetStackHelper stack;
   stack.Install (nodes);
 
   Ipv4AddressHelper address;
   address.SetBase ("10.1.1.0", "255.255.255.0");
-
   Ipv4InterfaceContainer interfaces = address.Assign (devices);
 
+  /* 4.Application Install */
   UdpEchoServerHelper echoServer (9);
 
   ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
@@ -67,6 +59,12 @@ main (int argc, char *argv[])
   clientApps.Start (Seconds (2.0));
   clientApps.Stop (Seconds (10.0));
 
+  /* 5.Simulator Setup */
+  Time::SetResolution (Time::NS);
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+
+  /* Run Simulator */
   Simulator::Run ();
   Simulator::Destroy ();
   return 0;
