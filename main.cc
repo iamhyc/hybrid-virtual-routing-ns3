6fefa35405d9742f8a1f92f806e3ef6975ec2f2d
iamhyc <sudofree@163.com>
2018-03-03 22:18:08 +0800

    update ns-script norm

--- main.cc
+++ main.cc
@@ -22,9 +22,54 @@ using namespace ns3;
 NS_LOG_COMPONENT_DEFINE ("HybridVirtualRouting");
 
 /*Global Static Configuration*/
-static NodeContainer nodes;
+// nodes
+static NodeContainer      p2pNodes,   csmaNodes,    wifiApNode, wifiStaNodes;
+static NetDeviceContainer p2pDevices, csmaDevices,  apDevices,  staDevices;
+// channel for topology
 static PointToPointHelper pointToPoint;
-static NetDeviceContainer devices;
+static CsmaHelper csma;
+static WifiHelper wifi;
+static WifiMacHelper mac;
+static MobilityHelper mobility;
+static InternetStackHelper stack;
+// network service
+static Ipv4InterfaceContainer p2pInterfaces, csmaInterfaces;
+static Ipv4AddressHelper address;
+
+void nodesInit()
+{
+  //init P2P nodes
+  p2pNodes.Create(2);
+  // init CSMA nodes
+  csmaNodes.Add (p2pNodes.Get(1));
+  csmaNodes.Create (3);
+  // init wifi nodes
+  wifiApNode = p2pNodes.Get (0);
+  wifiStaNodes.Create(3);
+}
+
+void topologyInit()
+{
+  //p2p channel
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  /* 1+2 Channel->Node */
+  p2pDevices = pointToPoint.Install (p2pNodes);
+  //wifi channel, {Area, Numbers, Range, Mobility, Loss, Delay}
+}
+
+void networkInit()
+{
+  // for p2p device
+  stack.Install (p2pNodes);
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  p2pInterfaces = address.Assign (p2pDevices);
+/*  // for csma device
+  stack.Install (csmaNodes);
+  // for wifi device
+  stack.Install (wifiApNode);
+  stack.Install (wifiStaNodes);*/
+}
 
 int main(int argc, char *argv[])
 {
@@ -32,42 +77,29 @@ int main(int argc, char *argv[])
 
   /* Command Parameter Parse */
   CommandLine cmd;
-  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+  cmd.AddValue ("verbose", "verbose and more", verbose);
   cmd.Parse (argc, argv);
 
   /* 1.Node Container Setup */
-  nodes.Create (2);
-
+  nodesInit();
   /* 2.Channel Helper Setup */
-  //p2p channel
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
-  //wifi channel, {Area, Numbers, Range, Mobility, Loss, Delay}
-
-  /* 1+2 Channel->Node */
-  devices = pointToPoint.Install (nodes);
-
+  topologyInit();
   /* 3.Internet Stack Install */
-  InternetStackHelper stack;
-  stack.Install (nodes);
-
-  Ipv4AddressHelper address;
-  address.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+  networkInit();
 
   /* 4.Application Install */
   UdpEchoServerHelper echoServer (9);
 
-  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
+  ApplicationContainer serverApps = echoServer.Install (p2pNodes.Get (1));
   serverApps.Start (Seconds (1.0));
   serverApps.Stop (Seconds (10.0));
 
-  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
+  UdpEchoClientHelper echoClient (p2pInterfaces.GetAddress (1), 9);
   echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
-  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+  ApplicationContainer clientApps = echoClient.Install (p2pNodes.Get (0));
   clientApps.Start (Seconds (2.0));
   clientApps.Stop (Seconds (10.0));
 
@@ -75,6 +107,7 @@ int main(int argc, char *argv[])
   Time::SetResolution (Time::NS);
   if (verbose)
   {
+    pointToPoint.EnablePcapAll("main-p2p");
     LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
     LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
   }
